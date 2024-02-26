a92e761ba57d7da05c808496041649e7cb112a8c
Bird-Forest <bird.forest70@gmail.com>
2024-02-26 08:25:39 +0200

    page

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <BrowserRouter basename="task-manager-authorization">
+      <BrowserRouter basename="pf-tasks-frontend">
         <App />
       </BrowserRouter>
     </PersistGate>
