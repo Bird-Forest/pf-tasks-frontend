60144bbb9f758608ffb2e5751dde7e2a0ae5297b
Bird-Forest <127691003+Bird-Forest@users.noreply.github.com>
2024-03-14 08:44:17 +0000

    Deploying to gh-pages from @ Bird-Forest/pf-tasks-frontend@e0c955c42d9870aede7969c08aaa51dd80d34c7f 🚀

--- static/js/520.7d836b4f.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[520],{4520:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});n(2791);var r=n(184);function u(){return(0,r.jsx)("div",{children:"NotFound"})}}}]);
-//# sourceMappingURL=520.7d836b4f.chunk.js.map
